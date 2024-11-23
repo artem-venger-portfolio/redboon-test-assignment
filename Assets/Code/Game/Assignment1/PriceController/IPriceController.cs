5b87f294ba79cad5590669e053307595c446e0e2
Artem Venger <flooptrue@gmail.com>
2024-11-23 12:10:23 +0700

    Removed the price controller

--- Assets/Code/Game/Assignment1/PriceController/IPriceController.cs
+++
@@ -1,8 +0,0 @@
-namespace Game.Assignment1.PriceController
-{
-    public interface IPriceController
-    {
-        int GetPurchasePrice(Items item);
-        int GetSellPrice(Items item);
-    }
-}
