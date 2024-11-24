045c40ab03a5574c655aa8878cc539176688c9bb
Artem Venger <flooptrue@gmail.com>
2024-11-24 20:54:28 +0700

    Handled the case when the lines points coincide

--- Assets/Code/Game.Unity/Assignment2/PathFinder/Geometry.cs
+++ Assets/Code/Game.Unity/Assignment2/PathFinder/Geometry.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Game.Unity.Assignment2
@@ -10,10 +11,23 @@ namespace Game.Unity.Assignment2
             var vector1 = line1End - line1Start;
             var vector2 = line2End - line2Start;
             
+            var denominator = (-vector2.x * vector1.y + vector1.x * vector2.y);
+            
+            if (line1Start == line2Start || line1Start == line2End ||
+                line1End == line2Start || line1End == line2End)
+            {
+                return true;
+            }
+
+            if (Mathf.Approximately(denominator, 0))
+            {
+                throw new InvalidOperationException("Cannot calculate intersection of parallel lines");
+            }
+            
             var s = (-vector1.y * (line1Start.x - line2Start.x) + vector1.x * (line1Start.y - line2Start.y)) / 
-                    (-vector2.x * vector1.y + vector1.x * vector2.y);
+                    denominator;
             var t = (vector2.x * (line1Start.y - line2Start.y) - vector2.y * (line1Start.x - line2Start.x)) / 
-                    (-vector2.x * vector1.y + vector1.x * vector2.y);
+                    denominator;
 
             return s >= 0 && s <= 1 && t >= 0 && t <= 1;
         }
